19cfc00363aa1f5495c94412dc2edc23b50b5697
Patterson Alt <pattersonalt@Pattersons-MacBook-Pro-2.local>
2022-11-14 13:55:52 -0500

    created Spring Boot base

---
+++ ICS-Services/src/main/java/com/cognixia/jump/IcsServicesApplication.java
@@ -0,0 +1,13 @@
+package com.cognixia.jump;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcsServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcsServicesApplication.class, args);
+	}
+
+}

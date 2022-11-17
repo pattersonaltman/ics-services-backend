130cde7abd044bcaccb8b7480c1c2e06aac6c935
SeanBryson <brysonseanw@gmail.com>
2022-11-17 16:42:06 -0600

    modified:   src/main/java/com/cognixia/jump/controller/PurchaseController.java 	modified:   src/main/java/com/cognixia/jump/repository/OrderRepository.java 	modified:   src/main/java/com/cognixia/jump/service/PurchaseService.java

--- ICS-Services/src/main/java/com/cognixia/jump/repository/OrderRepository.java
+++ ICS-Services/src/main/java/com/cognixia/jump/repository/OrderRepository.java
@@ -1,15 +1,16 @@
 package com.cognixia.jump.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.cognixia.jump.model.OrderItem;
+import com.cognixia.jump.model.User;
 
 @Repository
 public interface OrderRepository extends JpaRepository<OrderItem, Long> {
 
-	
-	
-	
+	public Optional<OrderItem> findByUser(User user);
 	
 }

3e837ed4b2e1690ef7f0e35b925fda5cd8b6476f
Patterson Alt <pattersonalt@Pattersons-MacBook-Pro-2.local>
2022-11-18 13:12:12 -0500

    OrderController: getOrderCheckout() ->  added functionality to ignore multiple values from CABLE & INTERNET; allow multiple unique values from STREAMING; some bug fixes

--- ICS-Services/src/main/java/com/cognixia/jump/controller/OrderController.java
+++ ICS-Services/src/main/java/com/cognixia/jump/controller/OrderController.java
@@ -31,6 +31,7 @@ import com.cognixia.jump.repository.OrderRepository;
 import com.cognixia.jump.repository.PurchaseRepository;
 import com.cognixia.jump.repository.ServiceRepository;
 import com.cognixia.jump.repository.UserRepository;
+import com.cognixia.jump.service.OrderService;
 import com.cognixia.jump.service.PurchaseService;
 
 @RestController
@@ -48,6 +49,12 @@ public class OrderController {
 	
 	@Autowired
 	ServiceRepository servRepo;
+	
+	@Autowired
+	UserRepository userRepo;
+	
+	@Autowired
+	OrderService orderService;
 	
 	//Get all orders
 	@GetMapping()
@@ -72,13 +79,7 @@ public class OrderController {
 	@PostMapping("/quote")
 	public ResponseEntity<?> getOrderQuote(@RequestBody Long[] ids) throws ResourceNotFoundException {
 		
-		double discount = ids.length * 0.1;
-		if(ids.length > 5)
-		{
-			discount = 0.5;
-		}
-		
-		OrderItem quote = new OrderItem(0L, null, ids.length, discount, 0, new HashSet<Purchase>());
+		OrderItem quote = new OrderItem(0L, null, 0, 0, 0, new HashSet<Purchase>());
 		
 		/* Parse @ids by Type and add into respective ArrayList */
 		List<Services> cable = new ArrayList<Services>();
@@ -109,6 +110,7 @@ public class OrderController {
 		
 		Purchase purchase;
 		double price;
+		double discount = 0;
 		
 		/* Delete all CABLE elements in List except for last element; add last element as purchase choice and calculate total */
 		int cableSize = cable.size() - 1;
@@ -120,6 +122,14 @@ public class OrderController {
 		quote.getPurchases().add(purchase);
 		price = cable.get(0).getPrice();
 		quote.setTotal(quote.getTotal() + price);
+		for(int i = 0; i < cable.size(); i++)
+		{
+			quote.setQty(quote.getQty() + 1);
+			discount += 1;
+		}
+		
+		
+		
 		
 		/* Delete all INTERNET elements in List except for last element; add last element as purchase choice and calculate total */
 		int internetSize = internet.size() - 1;
@@ -131,6 +141,11 @@ public class OrderController {
 		quote.getPurchases().add(purchase);
 		price = internet.get(0).getPrice();
 		quote.setTotal(quote.getTotal() + price);
+		for(int i = 0; i < internet.size(); i++)
+		{
+			quote.setQty(quote.getQty() + 1);
+			discount += 1;
+		}
 		
 		/* Add all STREAMING elements */
 		for(Services serv : streaming)
@@ -139,10 +154,142 @@ public class OrderController {
 			quote.getPurchases().add(purchase);
 			price = serv.getPrice();
 			quote.setTotal(quote.getTotal() + price);
+			quote.setQty(quote.getQty() + 1);
+			discount++;
 		}
+		
+		discount *= 0.1;
+		quote.setDiscount(discount);
 		
 		return ResponseEntity.status(200).body(quote);
 	}
+	
+	
+	
+	@PostMapping("/checkout2")
+	public ResponseEntity<?> getOrderCheckout2(@RequestBody Long[] ids) throws ResourceNotFoundException {
+		
+		OrderItem checkout = new OrderItem(null, null, 0, 0, 0, null);
+		repo.save(checkout);
+		
+		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		String username = ((UserDetails)principal).getUsername();
+		Optional<User> user = userRepo.findByUsername(username);
+		if (user.isPresent()) {
+			User currUser = user.get();
+			Optional<OrderItem> orderItem = repo.findByUser(currUser);
+			if (!orderItem.isPresent()) {
+				checkout = new OrderItem(null, currUser, ids.length, 0, 0, new HashSet<Purchase>());
+			}
+			else
+			{
+				Optional<OrderItem> opt = repo.findByUser(currUser);
+				if(opt.isPresent())
+				{
+					checkout = opt.get();
+				}
+			}
+			
+		
+//			OrderItem currOrder = orderItem.get();
+//			Optional<Services> service = servRepo.findById(serv_id);
+		
+		
+		
+			/* Parse @ids by Type and add into respective ArrayList */
+			List<Services> cable = new ArrayList<Services>();
+			List<Services> internet = new ArrayList<Services>();
+			Set<Services> streaming = new HashSet<Services>();		// STREAMING as a Set -> allow multiple, but only 1 of each
+			
+			for(int i = 0; i < ids.length; i++)
+			{
+				Optional<Services> opt = servRepo.findById(ids[i]);
+				if(opt.isPresent())
+				{
+					Services service = opt.get();
+					
+					if(service.getType() == Type.CABLE)
+					{
+						cable.add(service);
+					}
+					if(service.getType() == Type.INTERNET)
+					{
+						internet.add(service);
+					}
+					if(service.getType() == Type.STREAMING)
+					{
+						streaming.add(service);
+					}
+				}
+			}
+			
+			Purchase purchase;
+			double price;
+			double discount = 0;
+			
+			/* Delete all CABLE elements in List except for last element; add last element as purchase choice and calculate total */
+			int cableSize = cable.size() - 1;
+			for(int i = 0; i < cableSize; i++)
+			{
+				cable.remove(0);
+			}
+			repo.save(checkout);
+			purchase = purchaseService.createPurchaseCheckout(cable.get(0).getServ_id(), currUser, checkout);
+			checkout.getPurchases().add(purchase);
+			price = cable.get(0).getPrice();
+			checkout.setTotal(checkout.getTotal() + price);
+			for(int i = 0; i < cable.size(); i++)
+			{
+				checkout.setQty(checkout.getQty() + 1);
+				discount += 1;
+			}
+			
+			
+			/* Delete all INTERNET elements in List except for last element; add last element as purchase choice and calculate total */
+			int internetSize = internet.size() - 1;
+			for(int i = 0; i < internetSize; i++)
+			{
+				internet.remove(0);
+			}
+			repo.save(checkout);
+			purchase = purchaseService.createPurchaseCheckout(internet.get(0).getServ_id(), currUser, checkout);
+			checkout.getPurchases().add(purchase);
+			price = internet.get(0).getPrice();
+			checkout.setTotal(checkout.getTotal() + price);
+			for(int i = 0; i < cable.size(); i++)
+			{
+				checkout.setQty(checkout.getQty() + 1);
+				discount += 1;
+			}
+			
+			/* Add all STREAMING elements */
+			for(Services serv : streaming)
+			{
+				repo.save(checkout);
+				purchase = purchaseService.createPurchaseCheckout(serv.getServ_id(), currUser, checkout);
+				checkout.getPurchases().add(purchase);
+				price = serv.getPrice();
+				checkout.setTotal(checkout.getTotal() + price);
+				checkout.setQty(checkout.getQty() + 1);
+				discount++;
+			}
+			
+			discount *= 0.1;
+			checkout.setDiscount(discount);
+			
+			checkout.setQty((int) (discount * 10));
+			
+			repo.save(checkout);
+			
+			return ResponseEntity.status(200).body(checkout);
+		}
+		else {
+			throw new ResourceNotFoundException("User not found");
+		}
+		
+	}
+	
+	
 	
 	@PostMapping("/checkout")
 	public ResponseEntity<?> getOrderCheckout(@RequestBody Long[] ids) throws ResourceNotFoundException {
